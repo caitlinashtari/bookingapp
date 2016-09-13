f63cfd25a5dbb2b440c8df65a4ce88460da667a1
Anna Lovelace and Caitlin Ashtari <anna@>
2016-09-12 19:18:49 -0700

    update the booking website to include radio buttons, drop down menus, select boxes, a calendar and a color wheel.

--- js/scripts4.js
+++ js/scripts4.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
   $("#blanks form").submit(function(event){
     var personInput = $("input#person").val();
-    var dateInput = $("input#date").val();
     var startTimeInput = $("input#startTime").val();
     var endTimeInput = $("input#endTime").val();
+    var day = $("#day").val();
+    var service = $("input:radio[name=service]:checked").val();
+    var date = $("#appointment").val();
+    var color = $("#color").val();
 
     $('.person').text(personInput);
-    $('.date').text(dateInput);
     $('.startTime').text(startTimeInput);
     $('.endTime').text(endTimeInput);
     $('#confirmation').show();
